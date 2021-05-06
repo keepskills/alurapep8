4de82c9fc3271afcdef913c003f8f9b4e026deb4
keepskills <keepskills@protonmail.com>
2021-05-06 16:34:56 -0300

    alura_pep8

--- main.py
+++ main.py
@@ -2,17 +2,17 @@
 # coding=utf-8
 import os
 
+# from fila_normal import FilaNormal
+# from fila_prioritaria import FilaPrioritaria
 from fabrica_fila import FabricaFila
-from estatistica_detalhada import EstatisticaDetalhada
+# from estatistica_detalhada import EstatisticaDetalhada
 from estatistica_resumida import EstatisticaResumida
 
 os.system("cls||clear")
 
-teste_fabrica = FabricaFila.pega_fila('prioritaria')
-teste_fabrica.atualiza_fila()
-teste_fabrica.atualiza_fila()
-teste_fabrica.atualiza_fila()
-print(teste_fabrica.chama_cliente(10))
-print(teste_fabrica.chama_cliente(10))
-print(teste_fabrica.chama_cliente(10))
-print(teste_fabrica.estatistica(EstatisticaResumida('01/01/1991', 200)))
+fila = FabricaFila.pega_fila('prioritaria')
+fila.atualiza_fila()
+fila.atualiza_fila()
+fila.atualiza_fila()
+print(fila.chama_cliente(5))
+print(fila.estatistica(EstatisticaResumida('01/01/1991', 1234)))
